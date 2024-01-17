0a2699e9482b39949392d1937960ddba024e7012
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-01-17 09:42:12 +0800

    refine code

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,12 @@
-use etherparse::WriteError;
-use thiserror::Error;
-
 #[allow(dead_code)]
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum IpStackError {
     #[error("The transport protocol is not supported")]
     UnsupportedTransportProtocol,
     #[error("The packet is invalid")]
     InvalidPacket,
     #[error("Write error: {0}")]
-    PacketWriteError(WriteError),
+    PacketWriteError(etherparse::WriteError),
     #[error("Invalid Tcp packet")]
     InvalidTcpPacket,
     #[error("IO error: {0}")]
