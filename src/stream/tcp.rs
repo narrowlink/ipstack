f2a3344eb64c26066bc8b4216f5d536f3519899a
SajjadPourali <sajjad@pourali.com>
2024-03-27 23:08:08 -0400

    Disable auto shutdown in drop

--- src/stream/tcp.rs
+++ src/stream/tcp.rs
@@ -532,16 +532,8 @@ impl AsyncWrite for IpStackTcpStream {
 
 impl Drop for IpStackTcpStream {
     fn drop(&mut self) {
-        task::block_in_place(move || {
-            Handle::current().block_on(async move {
-                if matches!(self.tcb.get_state(), TcpState::Established) {
-                    _ = self.shutdown().await;
-                }
-
-                if let Ok(p) = self.create_rev_packet(NON, DROP_TTL, None, Vec::new()) {
-                    _ = self.packet_sender.send(p);
-                }
-            });
-        });
+        if let Ok(p) = self.create_rev_packet(NON, DROP_TTL, None, Vec::new()) {
+            _ = self.packet_sender.send(p);
+        }
     }
 }
